79b3866682eb6b61773d4cc393ccd04430f81711
tommytommy-ヒーロー <109708488+tommytommy-hero@users.noreply.github.com>
2022-08-02 22:56:57 +0000

    style.finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,15 +10,18 @@ class BooksController < ApplicationController
   end
 
   def create
-    book =Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
-
+    @book =Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books=Book.all
+      render :index
+    end
   end
 
   def show
     @book=Book.find(params[:id])
-
   end
 
   def edit
@@ -28,12 +31,20 @@ class BooksController < ApplicationController
 
   def update
     @book=Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+     flash[:success]="Book was successfully update."
+     redirect_to book_path(@book.id)
+    else
+      @books=Book.all
+      render :edit
+    end
 
   end
 
   def destroy
+    book=Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  post 'books'=>'books#create'
-
-  get 'books/:id'=>'books#show',as:'book'
-  get 'books/:id/edit'=>'books#edit',as:'edit_book'
-  get 'books/update'
-  patch 'books/:id'=>'books#update',as:'update_book'
-  get 'books/destroy'
-  get 'homes/top'
-
-  
+  root to:'homes#top'
+  resources :homes,:books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
